1f9d89b49362ace5fe47f2d34158f4305bf92d2c
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-08-10 19:51:46 +0100

    initializing a recipe project by John Thompson aka Spring Guru

---
+++ src/main/java/com/olakunle/GuruRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.olakunle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruRecipeApplication.class, args);
+    }
+
+}
