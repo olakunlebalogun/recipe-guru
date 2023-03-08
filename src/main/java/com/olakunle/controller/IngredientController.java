de2c44eb82c4fbf087535f51e294bfe5ba117ec5
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-03-08 13:08:52 +0100

    Encountered an issue trying to submit ingredient update

--- src/main/java/com/olakunle/controller/IngredientController.java
+++ src/main/java/com/olakunle/controller/IngredientController.java
@@ -1,6 +1,8 @@
 package com.olakunle.controller;
 
 import com.olakunle.commands.IngredientCommand;
+import com.olakunle.commands.RecipeCommand;
+import com.olakunle.commands.UnitOfMeasureCommand;
 import com.olakunle.services.IngredientService;
 import com.olakunle.services.RecipeService;
 import com.olakunle.services.UnitOfMeasureService;
@@ -60,5 +62,25 @@ public class IngredientController {
         log.debug("saved ingredient id:" + savedCommand.getId());
 
         return "redirect:/recipe/" + savedCommand.getRecipeId() + "/ingredient/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newIngredient(@PathVariable String recipeId, Model model){
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
+
+        return "recipe/ingredient/ingredientform";
     }
 }
